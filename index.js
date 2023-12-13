fd9d2e00afb5968bf226d0fa44d3acd4d361ec3f
Mell0w_Fell0w <mccoopton@gmail.com>
2023-12-13 08:51:03 -0700

    Login and home paths with login functionality v1

--- index.js
+++ index.js
@@ -7,3 +7,52 @@ const express = require('express');
 let app = express();
 
 let path = require('path');
+
+const users = [
+    { username: 'admin', password: 'pass123' }
+];
+
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: process.env.RDS_HOSTNAME || 'localhost',
+        user: process.env.RDS_USERNAME || 'postgres',
+        password: process.env.RDS_PASSWORD || 'Aliese0921',
+        database: process.env.RDS_BD_NAME || 'ebdb',
+        port: process.env.RDS_PORT || 5432,
+        ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
+    }
+});
+
+app.use(express.static('public'));
+
+//home path
+app.get('/', (req, res) => {
+    // to test if ejs is working
+    const name = 'John';
+    res.render('home', { name });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', { error: null });
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Simple array check for username and password
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        // Render the admin_data view with the fetched data
+        res.render('/admin');
+    } else {
+    res.render('login', { error: 'Invalid username or password' });
+}
+});
