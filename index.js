bb3e6e6cb1272e6d1febb7e2cbc5e8c393290b33
apeal2real <58010377+apeal2real@users.noreply.github.com>
2023-12-13 10:18:14 -0700

    index.js

--- index.js
+++ index.js
@@ -2,64 +2,102 @@
 //Section 1
 //This is our website to manage a database of tenant information for Wymount Community Aides
 
-const express = require("express");
+const express = require('express');
 
 let app = express();
 
-let path = require("path");
+let path = require('path');
 
-const users = [{ username: "admin", password: "pass123" }];
+const users = [
+    { username: 'admin', password: 'pass123' }
+];
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-// this is a change
+app.use( express.urlencoded( {extended: true}) );
 
-const knex = require("knex")({
-  client: "pg",
-  connection: {
-    host: process.env.RDS_HOSTNAME || "localhost",
-    user: process.env.RDS_USERNAME || "postgres",
-    password: process.env.RDS_PASSWORD || "Aliese0921",
-    database: process.env.RDS_BD_NAME || "ebdb",
-    port: process.env.RDS_PORT || 5432,
-    ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
-  },
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: 'localhost',
+        user: 'postgres',
+        password: 'Aliese0921',
+        database: 'tenants',
+        port: 1221
+    }
 });
 
-app.use(express.static("public"));
-
-//home path
-app.get("/", (req, res) => {
-  // to test if ejs is working
-  const name = "John";
-  res.render("home", { name });
+app.get('/', (req, res) => {
+    res.render('home', { error:null })
 });
 
-app.get("/login", (req, res) => {
-  res.render("login", { error: null });
+app.get('/login', (req, res) => {
+    res.render('login', { error:null })
 });
 
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
+app.get('/admin', (req, res) => {
+    knex.select().from('tenants').then( country => {
+        res.render('admin', { tenants : tenants});
+    })
+})
 
-  // Simple array check for username and password
-  const user = users.find(
-    (u) => u.username === username && u.password === password
-  );
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
 
-  if (user) {
-    // Render the admin_data view with the fetched data
-    res.render("/admin");
-  } else {
-    res.render("login", { error: "Invalid username or password" });
-  }
-});
+    // Simple array check for username and password
+    const user = users.find(u => u.username === username && u.password === password);
 
-//listen on the port specified above
-app.listen(port, () => {
-    console.log('Server is listening')
-});
+    if (user) {
+        // Render the admin_data view with the fetched data
+        knex.select().from('tenants').then( tenants => {
+            res.render('admin', { tenants : tenants});
+        })
+    } else {
+    res.render('login', { error: 'Invalid username or password' });
+}
+}); 
+
+// app.get("/editTenant/:id", (req, res)=> {
+//     knex.select("Apartment",
+//                     "FName", 
+//                     "LName", 
+//                     'Spouse_FName', 
+//                     'Spouse_LName', 
+//                     'PhoneNum', 
+//                     'Spouse_PhoneNum', 
+//                     'Email', 
+//                     'Spouse_Email').from("tenants").where("Apartment", req.params.apartment).then(tenants => {
+//     res.render("editTenant", {tenants: tenants});
+//    }).catch( err => {
+//       console.log(err);
+//       res.status(500).json({err});
+//    });
+//  });
+
+//  app.post("/editTenant", (req, res)=> {
+//     knex("country").where("country_id", parseInt(req.body.country_id)).update({
+//       country_name: req.body.country_name.toUpperCase(),
+//       popular_site: req.body.popular_site.toUpperCase(),
+//       capital: req.body.capital.toUpperCase(),
+//       population: req.body.population,
+//       visited: req.body.visited ? "Y" : "N",
+//       covid_level: req.body.covid_level.toUpperCase()
+//    }).then(mycountry => {
+//       res.redirect("/");
+//    })
+//  });
+
+//  app.post("/deleteCountry/:id", (req, res) => {
+//     knex("country").where("country_id",req.params.id).del().then( mycountry => {
+//       res.redirect("/");
+//    }).catch( err => {
+//       console.log(err);
+//       res.status(500).json({err});
+//    });
+//  });
+
+app.listen(3000, () => console.log('Server is running'));
